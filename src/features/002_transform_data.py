989cbd42a266091b3fd8bc48266990f06ff3cabb
martingeew <67925955+martingeew@users.noreply.github.com>
2023-10-22 22:18:59 +0200

    Add feature folder and transform file

---
+++ src/features/002_transform_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+data = pd.read_pickle("../../data/interim/ramen_data_by_country.pkl")
+
+# --------------------------------------------------------------
+# Transform data
+# --------------------------------------------------------------
+
+# Setting 'Month' as the index
+data.set_index('Month', inplace=True)
+
+data_moving_avg_12 = data.rolling(window=12).mean()
+
+# --------------------------------------------------------------
+# Export data
+# --------------------------------------------------------------
+
+data_moving_avg_12.to_pickle("../../data/processed/ramen_by_country_12_yr_ma.pkl")
