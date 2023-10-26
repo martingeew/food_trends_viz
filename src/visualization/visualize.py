74cbb1bd21df180139e89bb890361403b215f5e4
martingeew <67925955+martingeew@users.noreply.github.com>
2023-10-26 23:38:25 +0200

    Add text labels

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -5,9 +5,8 @@ import pandas as pd
 
 data = pd.read_pickle("../../data/processed/ramen_by_country_12_yr_ma.pkl")
 
-# change to ax subplot
-
-plt.figure(figsize=(15, 7))  # Adjust the figure size for better visualization
+# Initialize the plot
+fig, ax = plt.subplots(figsize=(14, 8.5))
 
 # Define custom colors for each country
 country_colors = {
@@ -20,29 +19,74 @@ country_colors = {
     # Add any other countries with their desired colors
 }
 
-xmin = data.index.min().year
-xmax = data.index.max().year + 4  # Adding 4 years to the maximum year for some padding on the right
-#plt.xlim(xmin, xmax)
-
-LABEL_Y = [data[country].iloc[-1] for country in data.columns]
-
-import matplotlib.pyplot as plt
-
-# Initialize the plot
-fig, ax = plt.subplots(figsize=(14, 8.5))
-
 # Plotting each country's data with assigned custom colors
 for country in data.columns:
     ax.plot(data.index, data[country], label=country, color=country_colors[country])
+    
+# Set size of plot to accomodate text labels
+xmin = data.index.min()
+xmax = data.index.max()+ pd.DateOffset(years=2) # add space to the right of the lines for labels
+ax.set_xlim(xmin, xmax)
+
+# Define the last value for each country
+# LABEL_Y = [data[country].iloc[-1] for country in data.columns]
+label_y = {
+    "New Zealand": 70.0,
+    "Germany": 83.75,  
+    "Australia": 75.58, 
+    "US": 88.0, 
+    "Netherlands": 93.0, 
+    "UK": 79.0,  
+}
+
+# Set the length for the lines to each label
+x_start = data.index.max()
+x_end = data.index.max()+ pd.DateOffset(years=1)
+
+# Add labels for countries iteratively
+for country in data.columns:
+    data_temp = data[[country]].iloc[-1:]
+    
+    # Country name
+    text=country
+    
+    # Colour for country label
+    color = country_colors[country]
+    
+    # Vertical start of line
+    y_start = data_temp.iat[0, 0]
+    # Vertical end of line
+    y_end = label_y[country]
+    
+    # Add line based on three points
+    ax.plot(
+        [x_start, (x_start + (x_end - x_start) / 2) , x_end], 
+        [y_start, y_end, y_end], 
+        color=color, 
+        alpha=0.5, 
+        ls="dashed"
+    )
+    
+    # Add country text
+    ax.text(
+        x_end, 
+        y_end, 
+        text, 
+        color=color, 
+        fontsize=14, 
+        weight="bold", 
+        fontfamily="Montserrat", 
+        va="center"
+    )
+
 
 # Setting labels, title, and other properties
 ax.set_xlabel('Month')
 ax.set_ylabel('12-Month Moving Average of Ramen Interest')
 ax.set_title('Ramen Interest Over Time (12-Month Moving Average)')
-ax.legend()
 ax.grid(True)
 ax.set_xticks(ax.get_xticks())  # This ensures that the x-ticks are set before rotation
-ax.set_xticklabels(ax.get_xticklabels(), rotation=45)  # Rotate x-axis labels for better visibility
+ax.set_xticklabels(ax.get_xticklabels())  # Rotate x-axis labels for better visibility
 
 plt.tight_layout()
 plt.show()
