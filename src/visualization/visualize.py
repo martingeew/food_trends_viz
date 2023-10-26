43a0d7cbd2f8fcdacf07ea5b8753515ef84f52ca
martingeew <67925955+martingeew@users.noreply.github.com>
2023-10-26 19:34:46 +0200

    Use ax subplot method

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -26,18 +26,25 @@ xmax = data.index.max().year + 4  # Adding 4 years to the maximum year for some 
 
 LABEL_Y = [data[country].iloc[-1] for country in data.columns]
 
+import matplotlib.pyplot as plt
+
+# Initialize the plot
+fig, ax = plt.subplots(figsize=(14, 8.5))
+
 # Plotting each country's data with assigned custom colors
 for country in data.columns:
-    plt.plot(data.index, data[country], label=country, color=country_colors[country])
+    ax.plot(data.index, data[country], label=country, color=country_colors[country])
 
-plt.xlabel('Month')
-plt.ylabel('12-Month Moving Average of Ramen Interest')
-plt.title('Ramen Interest Over Time (12-Month Moving Average)')
-plt.legend()
-plt.grid(True)
+# Setting labels, title, and other properties
+ax.set_xlabel('Month')
+ax.set_ylabel('12-Month Moving Average of Ramen Interest')
+ax.set_title('Ramen Interest Over Time (12-Month Moving Average)')
+ax.legend()
+ax.grid(True)
+ax.set_xticks(ax.get_xticks())  # This ensures that the x-ticks are set before rotation
+ax.set_xticklabels(ax.get_xticklabels(), rotation=45)  # Rotate x-axis labels for better visibility
+
 plt.tight_layout()
-plt.xticks(rotation=45)  # Rotate x-axis labels for better visibility, if needed
-
 plt.show()
 
 
