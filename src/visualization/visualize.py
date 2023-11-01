5c9d91ac1e3b5f5d46671ced4019f82afaec9924
martingeew <67925955+martingeew@users.noreply.github.com>
2023-11-01 22:03:54 +0100

    add data labels and styling

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -3,10 +3,48 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data = pd.read_pickle("../../data/processed/ramen_by_country_12_yr_ma.pkl")
+data = pd.read_pickle("../../data/processed/ramen_by_country_12_month_ma.pkl")
 
 # Initialize the plot
 fig, ax = plt.subplots(figsize=(14, 8.5))
+
+# --------------------------------------------------------------
+# Set styling
+# --------------------------------------------------------------
+
+# Colour pallete
+GREY10 = "#1a1a1a"
+GREY30 = "#4d4d4d"
+GREY40 = "#666666"
+GREY50 = "#7f7f7f"
+GREY60 = "#999999"
+GREY75 = "#bfbfbf"
+GREY91 = "#e8e8e8"
+GREY98 = "#fafafa"
+
+# Set background colours
+fig.patch.set_facecolor(GREY98) # sets colour of entire background (incl. outside the plot area)
+ax.set_facecolor(GREY98) # set colour of plot area
+
+# Customize spines
+ax.spines["right"].set_color("none")
+ax.spines["top"].set_color("none")
+ax.spines["left"].set_color(GREY91)
+ax.spines["bottom"].set_color(GREY91)
+
+ax.set_xticks(ax.get_xticks())  # This ensures that the x-ticks are set before rotation
+
+ax.set_xticklabels(ax.get_xticklabels())  # Rotate x-axis labels for better visibility
+
+# Set tick label font size to 12pt
+ax.tick_params(axis='both', which='major', labelsize=17)
+
+# Set tick colours
+ax.tick_params(axis='both', colors=GREY40)
+
+# --------------------------------------------------------------
+# Create line plot with annotated labels
+# --------------------------------------------------------------
 
 # Define custom colors for each country
 country_colors = {
@@ -73,20 +111,63 @@ for country in data.columns:
         y_end, 
         text, 
         color=color, 
-        fontsize=14, 
+        fontsize=15, 
         weight="bold", 
         fontfamily="Montserrat", 
         va="center"
     )
+    
+# --------------------------------------------------------------
+# Plot specific customizations
+# --------------------------------------------------------------
 
+# Define the start, end, and frequency of the x tick labels
+start = '2005-01-01'
+end = '2025-01-01'
+frequency = '24M'  # Every 2 years
+x_tick_labels = pd.date_range(start=start, end=end, freq=frequency)
 
-# Setting labels, title, and other properties
-ax.set_xlabel('Month')
-ax.set_ylabel('12-Month Moving Average of Ramen Interest')
-ax.set_title('Ramen Interest Over Time (12-Month Moving Average)')
-ax.grid(True)
-ax.set_xticks(ax.get_xticks())  # This ensures that the x-ticks are set before rotation
-ax.set_xticklabels(ax.get_xticklabels())  # Rotate x-axis labels for better visibility
+ax.set_xticks(x_tick_labels)
+ax.set_xticklabels(x_tick_labels.strftime('%Y'), weight=500,
+    color=GREY40) 
+
+# Customize the start, end, and frequency of the horizontal grid lines
+y_start = 0.0
+y_end = 100
+y_frequency = 10
+y_ticks = np.arange(y_start, y_end + y_frequency, y_frequency)
+
+ax.set_yticks(y_ticks)
+ax.set_yticklabels(y_ticks, weight=500,
+    color=GREY40) 
+
+ax.grid(True, axis='y', color=GREY91, lw=1.0)  # Ensure the horizontal grid is enabled
+
+fig.suptitle('Google searches for Ramen in selected countries',fontsize=30, x=0.065, ha='left')
+ax.set_title(
+    '12-month moving average, 2004-2023 (100 = peak popularity)',
+    loc='left',
+    fontsize=18,
+    pad=20
+    )
+
+ax.annotate(
+    '@mardywong',
+    xy = (1.0, -0.15),
+    xycoords='axes fraction',
+    ha='right',
+    va="center",
+    fontsize=14
+)
+
+ax.annotate(
+    'Source: Google trends',
+    xy = (0.0, -0.15),
+    xycoords='axes fraction',
+    ha='left',
+    va="center",
+    fontsize=14
+)
 
 plt.tight_layout()
 plt.show()
